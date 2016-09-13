abaaf335caca9e14082cb3fd853d498419c3543a
Shu Xu <sxu332@gatech.edu>
2016-09-13 16:52:03 -0500

    add UI interface for the App
    Initial the package
    UI interface for the App

---
+++ Assignment4/TipCalculator/app/src/main/java/edu/gatech/seclass/tipcalculator/TipCalculatorActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.seclass.tipcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TipCalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_calculator);
+    }
+}
